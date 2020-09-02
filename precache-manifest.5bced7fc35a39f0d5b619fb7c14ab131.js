840fdf53fbc376e1becb9b47579f8b770c8e3935
Wiki01 <jae930107@naver.com>
2020-09-02 21:15:57 +0900

    Updates

---
+++ precache-manifest.5bced7fc35a39f0d5b619fb7c14ab131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cb2fd59a54775546377c8e8a544d4a3",
+    "url": "/wiki01/movie_app_2020/index.html"
+  },
+  {
+    "revision": "bc9377f640e26fe36cb5",
+    "url": "/wiki01/movie_app_2020/static/css/main.fb5106b2.chunk.css"
+  },
+  {
+    "revision": "054b3461f86dc466ab2f",
+    "url": "/wiki01/movie_app_2020/static/js/2.5d94af01.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wiki01/movie_app_2020/static/js/2.5d94af01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc9377f640e26fe36cb5",
+    "url": "/wiki01/movie_app_2020/static/js/main.74c9853d.chunk.js"
+  },
+  {
+    "revision": "256fca60c8b5cdd03beb",
+    "url": "/wiki01/movie_app_2020/static/js/runtime-main.c74193a1.js"
+  }
+]);
